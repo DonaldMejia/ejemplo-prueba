41c4c99ce2edb40031fa3841d7d38f6b0bf8cf56
Ronald Mejia <ronaldmeji@gmail.com>
2022-06-15 02:16:31 -0500

    Se crea el modelo de negocio

---
+++ src/main/java/com/pichincha/prueba/demo/entity/OrderDetail.java
@@ -0,0 +1,31 @@
+package com.pichincha.prueba.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OrderDetail {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne
+	private Product product;
+	@ManyToOne
+	private Order order;
+	private Integer totalOdered;
+	private Double priceUnit;
+	private Double priceTotal;
+		
+}
